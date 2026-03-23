4f18f288d453e6e6dc9598f0256f896d391c11df
deysichisag9-sys <deysichisag9@gmail.com>
2026-03-23 21:48:28 +0100

    Guardando mis cambios locales

--- djangoapi/scripts/p1/main.py
+++ djangoapi/scripts/p1/main.py
@@ -1,9 +1,9 @@
 import sys
 
-from buildings.insert import insert as insert_building
-from buildings.select import select as select_building
-from buildings.update import update as update_building
-from buildings.delete import delete as delete_building
+from parcelas.insert import insert as insert_parcelas
+from parcelas.select import select as select_parcelas
+from parcelas.update import update as update_parcelas
+from parcelas.delete import delete as delete_parcelas 
 
 def main():
     # sys.argv[0] es siempre el nombre del archivo (main.py)
@@ -16,26 +16,27 @@ def main():
         sys.exit(0)
 
 
-    if tableName not in ["buildings", "trees", "water"]:
-        print("Error: The available table names are buildings, trees, water")
+    if tableName not in ["parcelas", "arboles", "caminos"]:
+        print("Error: The available table names are parcelas, arboles, caminos")
         sys.exit(0)
     
     if functionName not in ["insert", "select", "selectAsDict", "update", "delete"]:
         print("Error the available function names are insert, select, delete or update")
         sys.exit(0)
 
-    if tableName == "buildings":
+    if tableName == "parcelas":
         if functionName=="insert":
-            insert_building()
+            insert_parcelas()
         elif functionName=="select":
-            select_building()
+            select_parcelas()
         elif functionName=="selectAsDict":
-            select_building(asDict=True)
+            select_parcelas(asDict=True)
         elif functionName=="update":
-            update_building()
+            update_parcelas()
         elif functionName=="delete":
-            delete_building()
-    elif tableName=="trees":
+            delete_parcelas()
+            
+    elif tableName=="arboles":
         if functionName=="insert":
             pass
         elif functionName=="select":
@@ -43,6 +44,18 @@ def main():
         elif functionName=="update":
             pass
         elif functionName=="delete":
+            pass
+
+    elif tableName == "caminos":
+        if functionName == "insert":
+            pass 
+        elif functionName == "select":
+            pass
+        elif functionName == "selectAsDict":
+            pass
+        elif functionName == "update":
+            pass
+        elif functionName == "delete":
             pass
 
 if __name__ == "__main__":
