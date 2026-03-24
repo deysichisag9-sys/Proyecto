f035a93a77c44361d4ceb8af76652090c6a8985b
deysichisag9-sys <deysichisag9@gmail.com>
2026-03-24 19:08:43 +0100

    Guardando mis cambios locales

--- djangoapi/scripts/p1/parcelas/parcelas.py
+++ djangoapi/scripts/p1/parcelas/parcelas.py
@@ -6,26 +6,26 @@ class ParcelasOOP:
     def insert(self, d: dict):
         db = Db()
         
-        # 1. Redondear coordenadas a 0.0001
+        # Redondear las coord 
         db.query("SELECT ST_AsText(ST_SnapToGrid(ST_GeomFromText(%s, %s), 0.0001)) AS geom_snapped", 
                  [d['geom'], p1Settings.EPSG_CODE])
         geom_snapped = db.result[0]['geom_snapped']
         
-        # 2. Validar que el polígono esté bien dibujado
+        # para ver si el polgno este bien dibujado (OJITO)
         db.query("SELECT ST_IsValid(ST_GeomFromText(%s, %s)) AS is_valid", 
                  [geom_snapped, p1Settings.EPSG_CODE])
         if not db.result[0]['is_valid']:
             db.disconnect()
-            return {'ok': False, 'message': 'Geometria invalida', 'data': None}
+            return {'ok': False, 'message': 'Geometry invalid', 'data': None}
             
-        # 3. Comprobar que NO se interseque con otra parcela existente
+        # que no haya intersecciones  con alguna  parcela existente
         db.query("SELECT id FROM d.parcelas WHERE ST_Intersects(geom, ST_GeomFromText(%s, %s))", 
                  [geom_snapped, p1Settings.EPSG_CODE])
         if len(db.result) > 0:
             db.disconnect()
-            return {'ok': False, 'message': 'El poligono se interseca con parcelas existentes', 'data': db.result}
+            return {'ok': False, 'message': 'Existe interseccion poligono', 'data': db.result}
             
-        # 4. Insertar los datos
+        # 4. Insert  de los datos
         cons = """
             INSERT INTO d.parcelas (nombre, propietario, area_ha, tipo_bosque, estado_legal, geom)
             VALUES (%s, %s, %s, %s, %s, ST_GeomFromText(%s, %s))
@@ -41,7 +41,7 @@ class ParcelasOOP:
     def update(self, d: dict):
         db = Db()
         
-        # Redondear y validar igual que en insert
+        # Redondeo y validaccion  (algo similar que el insert) 
         db.query("SELECT ST_AsText(ST_SnapToGrid(ST_GeomFromText(%s, %s), 0.0001)) AS geom_snapped", [d['geom'], p1Settings.EPSG_CODE])
         geom_snapped = db.result[0]['geom_snapped']
         
@@ -50,14 +50,14 @@ class ParcelasOOP:
             db.disconnect()
             return {'ok': False, 'message': 'Geometria invalida', 'data': None}
             
-        # Comprobar intersecciones (¡excluyendo la parcela que estamos actualizando!)
+        # es para poder comprobar intersecciones nuevamente 
         db.query("SELECT id FROM d.parcelas WHERE ST_Intersects(geom, ST_GeomFromText(%s, %s)) AND id != %s", 
                  [geom_snapped, p1Settings.EPSG_CODE, d['id']])
         if len(db.result) > 0:
             db.disconnect()
-            return {'ok': False, 'message': 'El poligono se interseca con parcelas existentes', 'data': db.result}
+            return {'ok': False, 'message': 'interssecciones con poligonos que existen', 'data': db.result}
             
-        # Actualizar
+        # Ojito para Actualizar
         cons = """
             UPDATE d.parcelas 
             SET nombre=%s, propietario=%s, area_ha=%s, tipo_bosque=%s, estado_legal=%s, geom=ST_GeomFromText(%s, %s)
@@ -66,23 +66,23 @@ class ParcelasOOP:
         values = [d['nombre'], d['propietario'], d['area_ha'], d['tipo_bosque'], d['estado_legal'], geom_snapped, p1Settings.EPSG_CODE, d['id']]
         db.query(cons, values)
         
-        # El profesor pide que update devuelva 'rows_updated'
+        # Utilizacion de rows_updated (inf guardada)
         filas_actualizadas = db.result
         db.disconnect()
-        return {'ok': True, 'message': 'Data updated', 'data': [{'rows_updated': filas_actualizadas}]}
+        return {'ok': True, 'message': 'Data updated', 'data': [{'rows_updated': filas_actualizadas}]} #rows_updated(es para guardar el numero de registros / modificaciones)
 
     def delete(self, d: dict):
         db = Db()
         cons = "DELETE FROM d.parcelas WHERE id = %s"
         db.query(cons, [d['id']])
         
-        # El profesor pide que delete devuelva 'rows_deleted'
+        # Informacion eliminada rows_deleted
         filas_borradas = db.result
         db.disconnect()
         return {'ok': True, 'message': 'Data deleted', 'data': [{'rows_deleted': filas_borradas}]}
 
     def selectAsDicts(self, d: dict):
-        # Usamos getRowsAsDicts=True para que nos devuelva un diccionario automáticamente
+        # Usamos getRowsAsDicts=True para que  devuelva un diccionario (OJITO)
         db = Db(getRowsAsDicts=True)
         cons = """
             SELECT id, nombre, propietario, area_ha, tipo_bosque, estado_legal, ST_AsText(geom) as geom 
@@ -98,3 +98,4 @@ class ParcelasOOP:
             return {'ok': False, 'message': 'No data found', 'data': None}
             
         return {'ok': True, 'message': 'Data retrieved', 'data': data}
+    
--- djangoapi/scripts/p1/parcelas/parcelasDjango.py
+++ djangoapi/scripts/p1/parcelas/parcelasDjango.py
@@ -8,32 +8,32 @@ class ParcelasDjango:
     def insert(self, d: dict):
         cur = connection.cursor()
         
-        # 1. Redondear coordenadas a 0.0001 (Igual que el profesor en insert3)
+        # Coord para redondear 
         query = "SELECT ST_AsText(ST_SnapToGrid(ST_GeomFromText(%s, %s), 0.0001))"
         cur.execute(query, [d['geom'], p1Settings.EPSG_CODE])
         snapped_wkt_geometry = cur.fetchall()[0][0]
 
-        # 2. Validar con GEOSGeometry de Django
+        # observar la geomtria:  GEOSGeometry de Django
         g = GEOSGeometry(snapped_wkt_geometry, srid=p1Settings.EPSG_CODE)
         if not g.valid:
             return {'ok': False, 'message': 'Invalid geometry', 'data': None}
             
-        # 3. Comprobar intersecciones 
+        #  comprobaccion de las  interseccion 
         query_intersect = "SELECT id FROM d.parcelas WHERE ST_Intersects(geom, ST_GeomFromText(%s, %s))"
         cur.execute(query_intersect, [snapped_wkt_geometry, p1Settings.EPSG_CODE])
         if len(cur.fetchall()) > 0:
-            return {'ok': False, 'message': 'El poligono se interseca con parcelas existentes', 'data': None}
+            return {'ok': False, 'message': 'existe interseccion con el poligono', 'data': None}
             
-        # 4. ¡LA MAGIA DE DJANGO! Guardar en la base de datos
-        # Reemplazamos el texto de la geometría por el objeto GEOS válido
-        d['geom'] = g
-        b = Parcelas(**d) # Empaqueta todo el diccionario
-        b.save()          # Lo guarda automáticamente en PostgreSQL
         
-        # Devolvemos el diccionario exacto que pide el PDF
+        # agrega una componente espacial (es decir toma la inf de guarda de g)
+        d['geom'] = g
+        b = Parcelas(**d) # guarda  todo el diccionario
+        b.save()          #  guarda automaticament en PostgreSQL
+        
+        # Resultados 
         return {'ok': True, 'message': 'Data inserted', 'data': [{'id': b.id}]}
     
-    
+     #actualizar datos 
     def update(self, d: dict):
         cur = connection.cursor()
         query = "SELECT ST_AsText(ST_SnapToGrid(ST_GeomFromText(%s, %s), 0.0001))"
